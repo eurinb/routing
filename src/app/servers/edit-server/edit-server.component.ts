72e836e1e78582971427c9e59bba1c773a8f6316
Ingo Brenckmann <Ingo.Brenckmann@sap.com>
2018-01-15 15:40:43 +0100

    passing query parameters - Lecture 125

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 import { ServersService } from '../servers.service';
+
 
 @Component({
   selector: 'app-edit-server',
@@ -12,9 +14,17 @@ export class EditServerComponent implements OnInit {
   serverName = '';
   serverStatus = '';
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+
+    // Get Query Parameters from the request
+    // console.log(this.route.snapshot.queryParams);
+    // console.log(this.route.snapshot.fragment);
+    this.route.queryParams.subscribe();
+    this.route.fragment.subscribe();
+
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
